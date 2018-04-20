c14e70dca593038a76b010cd1defd3b2aff0c32e
Wembo Mulumba <fabriceotepa@hotmail.com>
2018-04-20 16:51:21 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList <Integer> arrayList = new ArrayList<Integer>();
+        LinkedList<Integer> linkedList = new LinkedList<Integer>() ;
+        doTimings("ArrayList", arrayList);
+        doTimings("LinkedList", linkedList);
+
+        System.out.println("Hello World!");
+    }
+
+    private static void doTimings(String type, List<Integer> list){
+
+        for (int i =0 ; i <1E5; i++){
+
+            list.add(i);
+        }
+
+        long start = System.currentTimeMillis();
+
+        // Add items at end of list
+        for(int i=0; i <1E5; i++){
+
+            list.add(i);
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println("Time Taken: " + (end -start) + " ms for" + type);
+    }
+}
